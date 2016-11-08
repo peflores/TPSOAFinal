7994dae63b9f3791decea6e06e80787a76bb9da4
peflores <fpe_m@yahoo.com.ar>
2016-11-08 02:44:20 -0300

    Se agregan librerias para trabajar con Web Services Rest y Gson

---
+++ app/src/main/java/trainner/soa/com/tpsoafinal/entidad/Arduino.java
@@ -0,0 +1,12 @@
+package trainner.soa.com.tpsoafinal.entidad;
+
+/**
+ * Created by Eze on 08/11/2016.
+ */
+public class Arduino {
+
+    /**
+     * falta definir el formato del json
+     */
+
+}
---
+++ app/src/main/java/trainner/soa/com/tpsoafinal/interfaces/ClienteService.java
@@ -0,0 +1,7 @@
+package trainner.soa.com.tpsoafinal.interfaces;
+
+/**
+ * Created by Eze on 08/11/2016.
+ */
+public interface ClienteService {
+}
